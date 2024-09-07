9bb8b028a8ddbd8571bdc921a24465c5a67428d7
Hepheoluwah <mtoyin27@gmail.com>
2024-09-07 19:45:27 +0100

    Updated Source Code

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: "src",
+  root: "src", // Your source directory
+  build: {
+    outDir: "../dist", // Output directory for the build
+    rollupOptions: {
+      input: {
+        main: "./index.html", // Main entry point
+      },
+    },
+  },
+  base: "/", // Base path for the deployment, adjust if deploying to a subdirectory
 });
